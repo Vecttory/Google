b989c7486f6723776457500ce367b00eded2f4db
Vecttory <alberto.ola.dev@gmail.com>
2023-07-26 17:32:40 -0600

    chore: preventing Links events

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link"
 import { TbGridDots } from "react-icons/tb"
 
@@ -5,13 +7,13 @@ const HomeHeader = () => {
   return (
     <header className="flex text-sm justify-end">
       <div className="flex space-x-4 p-4 items-center" >
-        <Link href={"/gmail"} className="hover:underline">
+        <a href={"/gmail"} onClick={e => e.preventDefault()} className="hover:underline">
           Gmail
-        </Link>
-        <Link href={"/images"} className="hover:underline">
+        </a>
+        <Link href={"/images"} onClick={e => e.preventDefault()} className="hover:underline">
           Images
         </Link>
-        <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2"/>
+        <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2 cursor-pointer"/>
         <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow">Sign in</button>
       </div>
     </header>
